9d1aaa65e64c429c93918cd2057c314d8ab417b5
Ernest <ernestcosta@gmail.com>
2017-05-19 20:12:36 +0200

    Script "transformer.R" para trabajar con el fichero "data.json".

---
+++ transformer.R
@@ -0,0 +1,33 @@
+# ********************
+# * Transformaciones *
+# ********************
+
+# Cargando Libreria
+library(jsonlite)
+
+# Creando objeto json
+json_file <- fromJSON("./data/data.json", flatten = TRUE )
+is.data.frame(json_file)
+
+# Convertimos el json en DataFrame
+df_json <- data.frame(do.call("rbind", json_file))
+is.data.frame(df_json)
+print(df_json)
+
+# Miramos si hay URLs duplicadas
+duplicated(df_json)
+
+# Eliminamos las duplicadas
+df_json_dd <- unique(df_json)
+print(df_json_dd)
+
+# Cambiamos las versiones -1 por NA
+df_json_dd$version[which(df_json_dd$version==-1)]<-NA
+print(df_json_dd)
+
+# Sumamos Numero de URLs de cada version
+table(df_json_dd)
+TotalVersiones <- colSums(table(df_json_dd))
+TotalVersiones
+
+
