42478db3a5b93b13964caefe949bc35f8f938a76
carlosrive94 <carlosrive94@gmail.com>
2017-05-26 18:10:32 +0200

    Update crawler & data

--- crawler/cse-requester.py
+++ crawler/cse-requester.py
@@ -5,8 +5,9 @@ from bs4 import BeautifulSoup
 
 apikey = 'AIzaSyClCn_nNkXcxnsdpXxTCG_LwVff66W4D7g'
 cx = '016149970605689745710:lzye9oapw7y'
-query = '%E2%80%9Cindex+of%E2%80%9D+inurl:wp-content'
-number_of_pages = 8
+query = '%E2%80%9Cindex+of%E2%80%9D+inurl%3Awp-content+site%3A'
+sites = ['es', 'ad', 'at', 'be', 'ch', 'cy', 'cz', 'de', 'dk', 'fl', 'fr', 'gr', 'ht', 'hu', 'ie', 'is', 'it', 'lu',
+         'ma', 'mc', 'nl', 'no', 'pt', 'se', 'uk']
 
 
 def get_version(domain):
@@ -16,21 +17,36 @@ def get_version(domain):
         soup = BeautifulSoup(f.read(), 'html.parser')
         version = soup.find('h1', {'id': 'logo'}).text.split(' ')[2].rstrip()
     except Exception as e:
-        print(e)
+        print('Error on getting version of ' + domain + ':', e)
         version = '-1'
     return version
 
 
-data = []
+data = {'urls': {}}
 
-for i in range(number_of_pages):
-    req_url = 'https://www.googleapis.com/customsearch/v1?key=' + apikey + '&cx=' + cx + '&q=' + query + '&start=' + str(
-        (i * 10) + 1)
-    resp = requests.get(req_url).json()
-    for item in resp['items']:
-        domain = item['displayLink']
-        version = get_version(domain)
-        data.append({'url': domain, 'version': version})
+for site in sites:
+    url = 'https://www.googleapis.com/customsearch/v1?key=' + apikey + '&cx=' + cx + '&q=' + query + site
+    data['urls'][site] = []
+
+    index = 1
+    while index != -1:
+        req_url = url + '&start=' + str(index)
+        print('Request to: ' + req_url)
+        try:
+            resp = requests.get(req_url).json()
+            for item in resp['items']:
+                domain = item['displayLink']
+                version = get_version(domain)
+                print('Adding ' + domain + ' / ' + version)
+                data['urls'][site].append({'url': domain, 'version': version})
+        except:
+            pass
+
+        try:
+            index = resp['queries']['nextPage'][0]['startIndex']
+        except:
+            index = -1
 
 with open("..\data\data.json", "w") as outfile:
-    json.dump({'urls': data}, outfile, indent=4)
+    json.dump(data, outfile, indent=4)
+    print('Data loaded on data.json')
